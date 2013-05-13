488ef68bd9e7fbd2421df295e426789e7b23f117
Mark Mruss <mark@indigorose.com>
2013-05-13 11:58:37 -0500

    Init fark

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'efa936288e6ade40a894fe197a84a23855f186aeed77358755055bb57f4e8252d487495a8abf2911e410c5da6f7667f1a50d9b5e47baf6dacb1e2f5b30eed5ce'
